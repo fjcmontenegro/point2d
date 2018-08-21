62b857e6f23b4af42a2ddae731de6831050b2a66
Fabricio JC Montenegro <fabriciojcmontenegro@gmail.com>
2018-08-21 09:58:10 -0300

    Setters created

--- point2d.py
+++ point2d.py
@@ -19,38 +19,50 @@ class Point2D:
             self._y = y
             self._r = math.sqrt(x**2 + y**2)
             self._a = math.atan2(y, x)
-        elif type(x) == tuple:
-            # creating from x and y as tuple
-            self._x = x[0]
-            self._y = x[1]
-            self._r = math.sqrt(x[0]**2 + x[1]**2)
-            self._a = math.atan2(x[1], x[0])
         elif not r == None and not a == None:
             # creating from r and a
             self._r = r
             self._a = a
             self._x = r * math.cos(a)
             self._y = r * math.sin(a)
+        elif type(x) == tuple:
+            # creating from x and y as tuple
+            self._x = x[0]
+            self._y = x[1]
+            self._r = math.sqrt(x[0]**2 + x[1]**2)
+            self._a = math.atan2(x[1], x[0])
 
     @property
     def x(self):
         return self._x
+
+    @x.setter
+    def x(self, val):
+        self._x = val
     
     @property
     def y(self):
         return self._y
+
+    @y.setter
+    def y(self, val):
+        self._y = val
     
     @property
     def r(self):
         return self._r
+
+    @r.setter
+    def r(self, val):
+        self._r = val
     
     @property
     def a(self):
         return self._a
-    
 
-
-
+    @a.setter
+    def a(self, val):
+        self._a = val
 
     def __repr__(self):
         return "Point2D({}, {})({}, {})".format(self._x,self._y,self._r,self._a)
@@ -58,7 +70,11 @@ class Point2D:
 if __name__ == '__main__':
     p  = Point2D((1,0))
     print(p)
-    print(p.x)
-    print(p.y)
-    print(p.r)
-    print(p.a)
+    p.x = 5
+    print(p)
+    p.y = 5
+    print(p)
+    p.r = 5
+    print(p)
+    p.a = math.pi
+    print(p)
