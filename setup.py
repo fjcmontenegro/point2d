ac824ab64c99a7ea80d20e16a84217c68e2244d0
SplinterDev <fabriciojcmontenegro@gmail.com>
2018-08-24 14:41:22 -0300

    Package ready for distribuition.

--- setup.py
+++ setup.py
@@ -2,4 +2,9 @@ from distutils.core import setup
 
 setup(name="point2d",
       version='1.0',
-      py_modules=['math']
+      py_modules=["point2d"],
+      description='2D point class to represent cartesian and polar coordinates.',
+      author='Fabricio JC Montenegro',
+      author_email='fabriciojcmontenegro@gmail.com',
+      url='https://github.com/SplinterDev/point2d',
+      )
