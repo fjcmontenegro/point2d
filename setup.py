e21d1c63f08479946612196bf01a80dadb71fc4a
SplinterDev <fabriciojcmontenegro@gmail.com>
2018-12-07 08:39:35 -0200

    Changed 'setup.py' to use setuptools instead of distutils.

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(name="point2d",
-      version='1.0',
-      py_modules=["point2d"],
-      description='2D point class to represent cartesian and polar coordinates.',
-      author='Fabricio JC Montenegro',
-      author_email='fabriciojcmontenegro@gmail.com',
-      url='https://github.com/SplinterDev/point2d',
-      )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="point2d",
+    version="1.0",
+    author="Fabricio J.C. Montenegro",
+    author_email="fabriciojcmontenegro@gmail.com",
+    description="2D point class to represent cartesian and polar coordinates seemlessly.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SplinterDev/point2d",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+)
